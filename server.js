7eb31c5ada714637ae3904a65c8a2efa49cb5aa9
DESKTOP-OV192TK\yonlu <ygoh019@e.ntu.edu.sg>
2018-07-30 21:03:03 +0800

    Updated redundancies

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var express=require ('express');
 var app=express();
 var server=require('http').createServer(app);
 var io= require('socket.io').listen(server);
-users= [];
-connections= [];
-
-server.listen(process.env.PORT||3000);
-console.log('Server running...');
-app.get('/', function(req, res){
+users=[];
+connections=[];
+server.listen(process.env.PORT|| 3000);
+console.log("Server running...");
+app.get('/', function(req,res){
+	
 	res.sendFile(__dirname+'/index.html');
-
-
 });
 
 io.sockets.on('connection', function(socket){
@@ -18,13 +16,14 @@ io.sockets.on('connection', function(socket){
 	console.log('Connected: %s sockets connected', connections.length);
 	
 	socket.on('disconnect', function(data){
+		
 		connections.splice(connections.indexOf(socket),1);
-		console.log('Disconnected: %s sockets connected', connections.length);
+		console.log("Disconnected: %s sockets connected", connections.length);
+		
 	});
 	
 	socket.on('send message', function(data){
 		console.log(data);
 		io.sockets.emit('new message', {msg:data});
-	});
-
+});
 });
